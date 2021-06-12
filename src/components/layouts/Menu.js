cafba12abdd2dd8ec723dc01773b65f22b8727e4
chemazu <chemazu>
2021-06-12 16:51:03 +0100

    front end complete

--- src/components/layouts/Menu.js
+++ src/components/layouts/Menu.js
@@ -1,62 +1,97 @@
-import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 class Menu extends Component {
-    render() {
-        return (
-            <Fragment>
-                {/* Logo */}
-                <Link className="navbar-brand" to="/"> <img src={process.env.PUBLIC_URL + "/assets/img/logo.png"} alt="logo" /> </Link>
-                {/* Menu */}
-                <ul className="navbar-nav">
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/">Home</Link>
-
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Buy</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-details">find properties for sale</Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Rent</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-details">find properties for rent  </Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Sell</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-details">sell with ease </Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Find Craftsmen</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-archive">Find craftsmen and other Pros</Link> </li>
-                            <li className="menu-item"> <Link to="/agency-details">“Enlist your trade as a craftsman</Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Request</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-archive">Agency Archive</Link> </li>
-                            <li className="menu-item"> <Link to="/agency-details">Agency Details</Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item menu-item-has-children">
-                        <Link to="/agency-archive">Agents</Link>
-                        <ul className="submenu">
-                            <li className="menu-item"> <Link to="/agency-archive">Estate Agents</Link> </li>
-                            <li className="menu-item"> <Link to="/agency-details">Developers</Link> </li>
-                        </ul>
-                    </li>
-                    
-                </ul>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        {/* Logo */}
+        <Link className="navbar-brand" to="/">
+          {" "}
+          <img
+            src={process.env.PUBLIC_URL + "/assets/img/logo.png"}
+            alt="logo"
+          />{" "}
+        </Link>
+        {/* Menu */}
+        <ul className="navbar-nav">
+          <li className="menu-item menu-item-has-children">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link>Buy</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/listing-grid">find properties for sale</Link>{" "}
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link>Rent</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/listing-grid">find properties for rent </Link>{" "}
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link to="/agency-archive">Sell</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/listing-grid">sell with ease </Link>{" "}
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link to="/agency-archive">Find Craftsmen</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-archive">
+                  Find craftsmen and other Pros
+                </Link>{" "}
+              </li>
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-details">
+                  “Enlist your trade as a craftsman
+                </Link>{" "}
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link to="/agency-archive">Request</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-archive">Agency Archive</Link>{" "}
+              </li>
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-details">Agency Details</Link>{" "}
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item menu-item-has-children">
+            <Link to="/agency-archive">Agents</Link>
+            <ul className="submenu">
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-archive">Estate Agents</Link>{" "}
+              </li>
+              <li className="menu-item">
+                {" "}
+                <Link to="/agency-details">Developers</Link>{" "}
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
 }
 
 export default Menu;
--- src/components/sections/home/Content.js
+++ src/components/sections/home/Content.js
@@ -1,34 +1,34 @@
-import React, { Component, Fragment } from 'react';
-import Bluecta from '../../layouts/Bluecta';
-import Agentsslider from './Agentsslider';
-import Banner from './Banner';
-import Categories from './Categories';
-import Latestblog from './Latestblog';
-import Listingslider from './Listingslider';
-import Search from './Search';
-import Singleagent from './Singleagent';
-import Testimonials from './Testimonials';
-import Toplistings from './Toplistings';
+import React, { Component, Fragment } from "react";
+import Bluecta from "../../layouts/Bluecta";
+import Agentsslider from "./Agentsslider";
+import Banner from "./Banner";
+import Categories from "./Categories";
+import Latestblog from "./Latestblog";
+import Listingslider from "./Listingslider";
+import Search from "./Search";
+import Singleagent from "./Singleagent";
+import Testimonials from "./Testimonials";
+import Toplistings from "./Toplistings";
 
 class Content extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Banner/>
-                <Search/>
-                <Categories/>
-                <Listingslider/>
-                <Toplistings/>
-                <Singleagent/>
-                <Agentsslider/>
-                <Latestblog/>
-                <Testimonials/>
-                <div className="section pt-0">
-                    <Bluecta/>
-                </div>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <Banner />
+        <Search />
+        <Categories />
+        <Listingslider />
+        <Toplistings />
+        {/* <Singleagent/> */}
+        {/* <Agentsslider/> */}
+        {/* <Latestblog/> */}
+        <Testimonials />
+        <div className="section pt-0">
+          <Bluecta />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Content;
